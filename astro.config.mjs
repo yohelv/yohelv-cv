a38bc0316f39541b4b93abae45b7b7bbc446270d
Yohel <yohelv@gmail.com>
2024-06-28 05:53:42 +0000

    yohelv-cv

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://yohelv.github.io/yohelv-cv-github.io',
-    base: 'yohelv-cv-github.io',
+    site: 'https://yohelv.github.io/yohelv-cv',
+    base: 'yohelv-cv',
 });
